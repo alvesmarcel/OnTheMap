6273a571a007b595b7a7d4dee56ce38ccaab06e6
Marcel Oliveira Alves <alvesmarcel@Marcels-MBP.home>
2015-09-01 11:56:46 -0300

    List View first version

--- OnTheMap/ListViewController.swift
+++ OnTheMap/ListViewController.swift
@@ -6,5 +6,37 @@
 //  Copyright (c) 2015 Marcel Oliveira Alves. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+	
+	@IBOutlet weak var tableView: UITableView!
+	
+	override func viewWillAppear(animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		self.tableView.reloadData()
+	}
+	
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		/* Get cell type */
+		let cellReuseIdentifier = "ListViewTableCell"
+		let student = ParseClient.sharedInstance().studentLocations[indexPath.row]
+		var cell = tableView.dequeueReusableCellWithIdentifier(cellReuseIdentifier) as! UITableViewCell
+		
+		/* Set cell defaults */
+		cell.textLabel!.text = "\(student.firstName) \(student.lastName)"
+		cell.imageView!.image = UIImage(named: "pin")
+		cell.imageView!.contentMode = UIViewContentMode.ScaleAspectFit
+		
+		return cell
+	}
+	
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return ParseClient.sharedInstance().studentLocations.count
+	}
+	
+	func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+		
+	}
+}
