89ba91f51ddeca5c66e576c7a0d7dbb57bfb5124
Marcel Oliveira Alves <alvesmarcel@Marcels-MBP.home>
2015-09-02 17:10:54 -0300

    JSONConvenience commented

--- OnTheMap/JSONConvenience.swift
+++ OnTheMap/JSONConvenience.swift
@@ -5,13 +5,14 @@
 //  Created by Marcel Oliveira Alves on 8/31/15.
 //  Copyright (c) 2015 Marcel Oliveira Alves. All rights reserved.
 //
+//  This class contains some methods to help with JSON data that is returned in the API calls
+//  All methods were taken from Udacity The Movie Manager app (Created by Jarrod Parkes on 2/11/15)
 
 import Foundation
 
 class JSONConvenience {
-	// MARK - Helpers
 	
-	/* Helper: Given raw JSON, return a usable Foundation object */
+	/* Given raw JSON data, return a usable Foundation object */
 	class func parseJSONWithCompletionHandler(data: NSData, completionHandler: (result: AnyObject!, error: NSError?) -> Void) {
 		
 		var parsingError: NSError? = nil
@@ -25,6 +26,7 @@ class JSONConvenience {
 		}
 	}
 	
+	/* Given a dictionary of parameters, return a string that will compose the URL  */
 	class func escapedParameters(parameters: [String : AnyObject]) -> String {
 		
 		var urlVars = [String]()
