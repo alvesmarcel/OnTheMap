e9c311c4ba5300468c7b92fa87fb901d32806e38
Marcel Oliveira Alves <alvesmarcel@Marcels-MBP.home>
2015-09-02 21:40:05 -0300

    ParseConstants comments added

--- OnTheMap/ParseConstants.swift
+++ OnTheMap/ParseConstants.swift
@@ -10,25 +10,32 @@
 
 extension ParseClient {
 	
+	// MARK: - Constants
 	struct Constants {
 
-		// URLs
+		// MARK: - URLs
 		static let ParseBaseUrl : String = "https://api.parse.com/1/classes/"
 		
+		// MARK: - AppID and APIKey
 		static let AppID = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
-		static let ApiKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+		static let APIKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
 	}
 	
+	// MARK: - Methods
 	struct Methods {
+		
 		static let StudentLocation : String = "StudentLocation"
 	}
 	
+	// MARK: - Parameter Keys
 	struct ParameterKeys {
 		static let Limit = "limit"
 		static let Skip = "skip"
 	}
 	
+	// MARK: - JSON Body Keys
 	struct JSONBodyKeys {
+		
 		static let ObjectID = "objectId"
 		static let UniqueKey = "uniqueKey"
 		static let FirstName = "firstName"
@@ -39,10 +46,12 @@ extension ParseClient {
 		static let Longitude = "longitude"
 	}
 	
+	// MARK: - JSON Response Keys
 	struct JSONResponseKeys {
 		static let Results = "results"
 	}
 	
+	// MARK: - HTTP Header Fields
 	struct HTTPHeaderField {
 		static let AppID = "X-Parse-Application-Id"
 		static let ApiKey = "X-Parse-REST-API-Key"
