2c72d6e381df0a39b8902529f23ae3b8ffca253b
Marcel Oliveira Alves <alvesmarcel@Marcels-MacBook-Pro.local>
2015-09-15 10:00:03 -0300

    taskForDELETEMethod implemented

--- OnTheMap/UdacityClient.swift
+++ OnTheMap/UdacityClient.swift
@@ -60,16 +60,63 @@ class UdacityClient : NSObject {
 		let urlString = Constants.UdacityBaseURL + method + JSONConvenience.escapedParameters(mutableParameters)
 		let url = NSURL(string: urlString)!
 		let request = NSMutableURLRequest(URL: url)
-		var jsonifyError: NSError? = nil
 		request.HTTPMethod = "POST"
 		request.addValue("application/json", forHTTPHeaderField: "Accept")
 		request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+		
+		var jsonifyError: NSError? = nil
 		request.HTTPBody = NSJSONSerialization.dataWithJSONObject(jsonBody, options: nil, error: &jsonifyError)
 		
 		let session = NSURLSession.sharedSession()
 		
 		/* 4. Make the request */
 		let task = session.dataTaskWithRequest(request) {data, response, downloadError in
+			
+			/* 5/6. Parse the data and use the data (happens in completion handler) */
+			if let error = downloadError {
+				completionHandler(result: nil, error: error)
+			} else {
+				
+				/* The first 5 bytes should be ignored, according to the documentation */
+				let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5))
+				
+				JSONConvenience.parseJSONWithCompletionHandler(newData, completionHandler: completionHandler)
+			}
+		}
+		
+		/* 7. Start the request */
+		task.resume()
+		
+		return task
+	}
+	
+	// MARK: - DELETE
+	
+	func taskForDELETEMethod(method: String, parameters: [String : AnyObject], jsonBody: [String:AnyObject], completionHandler: (result: AnyObject!, error: NSError?) -> Void) -> NSURLSessionDataTask {
+		
+		/* 1. Set the parameters */
+		var mutableParameters = parameters
+		
+		/* 2/3. Build the URL and configure the request */
+		let urlString = Constants.UdacityBaseURL + method + JSONConvenience.escapedParameters(mutableParameters)
+		let url = NSURL(string: urlString)!
+		let request = NSMutableURLRequest(URL: url)
+		request.HTTPMethod = "DELETE"
+		
+		/* Finding the cookie and setting the request value */
+		var xsrfCookie: NSHTTPCookie? = nil
+		let sharedCookieStorage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
+		for cookie in sharedCookieStorage.cookies as! [NSHTTPCookie] {
+			if cookie.name == "XSRF-TOKEN" { xsrfCookie = cookie }
+		}
+		if let xsrfCookie = xsrfCookie {
+			request.setValue(xsrfCookie.value!, forHTTPHeaderField: "X-XSRF-TOKEN")
+		}
+		
+		let session = NSURLSession.sharedSession()
+		
+		/* 4. Make the request */
+		let task = session.dataTaskWithRequest(request) { data, response, downloadError in
 			
 			/* 5/6. Parse the data and use the data (happens in completion handler) */
 			if let error = downloadError {
